4ca145b03da34ef3b3351cbb64c584596de4719e
LynnVlasenko <lynn.vlasenko@gmail.com>
2024-04-23 17:45:24 +0200

    Added HomeWork19 project

---
+++ HomeWork19/HomeWork19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork19
+//
+//  Created by Алина Власенко on 23.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
